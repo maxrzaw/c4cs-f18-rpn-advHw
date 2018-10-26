2857fd8d1d07642fccc1666d96e876fc89478577
Max Zawisa <maxrzaw@umich.edu>
2018-10-26 11:01:40 -0400

    Initial commit
    Initial code with Mkaefile and .gitignore

---
+++ test_rpn.py
@@ -0,0 +1,13 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate('1 1 +')
+        self.assertEqual(2, result)
+
+    def test_add2(self):
+        result = rpn.calculate('1 3 +')
+        self.assertEqual(4, result)
+
+
