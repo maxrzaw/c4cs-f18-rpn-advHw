1d99bfb32244c5b11ab0c6be08c5ff000868090a
Max Zawisa <maxrzaw@umich.edu>
2018-10-26 11:24:56 -0400

    Added tests for subtract

--- test_rpn.py
+++ test_rpn.py
@@ -6,8 +6,16 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
 
-#    def test_add2(self):
-#        result = rpn.calculate('1 3 +')
-#        self.assertEqual(4, result)
+    def test_add2(self):
+        result = rpn.calculate('1 3 +')
+        self.assertEqual(4, result)
 
 
+    def test_sub(self):
+        result = rpn.calculate('3 1 -')
+        self.assertEqual(2, result)
+
+        
+    def test_sub2(self):
+        result = rpn.calculate('1 3 -')
+        self.assertEqual(-2, result)
