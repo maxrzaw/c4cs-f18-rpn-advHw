15d09887ab1f71098f49955c4e13bc21199ad3c4
Max Zawisa <maxrzaw@umich.edu>
2018-10-26 13:13:46 -0400

    Added tests for the ^ operator

--- test_rpn.py
+++ test_rpn.py
@@ -33,7 +33,16 @@ class TestBasics(unittest.TestCase):
     def test_div2(self):
         result = rpn.calculate('3 3 /')
         self.assertEqual(1, result)
+
     def test_chain(self):
         result = rpn.calculate('1 2 + 1 +')
         self.assertEqual(4, result)
+
+    def test_exp(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
+
+    def test_exp2(self):
+        result = rpn.calculate('123 0 ^')
+        self.assertEqual(1, result)
 
