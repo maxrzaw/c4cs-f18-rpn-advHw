81309ff736b84831485a7d4a5af82fd386451739
Max Zawisa <maxrzaw@umich.edu>
2018-10-26 11:33:25 -0400

    Added tests for multiplication and division
    They are not passing

--- test_rpn.py
+++ test_rpn.py
@@ -10,12 +10,27 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('1 3 +')
         self.assertEqual(4, result)
 
-
     def test_sub(self):
         result = rpn.calculate('3 1 -')
         self.assertEqual(2, result)
-
         
     def test_sub2(self):
         result = rpn.calculate('1 3 -')
         self.assertEqual(-2, result)
+
+    def test_mul(self):
+        result = rpn.calculate('1 1 *')
+        self.assertEqual(1, result)
+
+    def test_mul2(self):
+        result = rpn.calculate('1 3 *')
+        self.assertEqual(3, result)
+
+    def test_div(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2, result)
+        
+    def test_div2(self):
+        result = rpn.calculate('-3 3 -')
+        self.assertEqual(-1, result)
+
